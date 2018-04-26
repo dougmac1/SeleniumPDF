5641b284e5684f76b500062a8200a54aa4db44c3
Douglas McCallum <douglas.mccallum@digital.hmrc.gov.uk>
2018-04-26 13:36:37 +0100

    test passes, loads page correctly

--- src/test/scala/SeleniumTestSpec.scala
+++ src/test/scala/SeleniumTestSpec.scala
@@ -1,3 +1,26 @@
-class SeleniumTestSpec {
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.scalatest.selenium.WebBrowser
+import org.scalatest.{MustMatchers, WordSpec}
 
+class SeleniumTestSpec extends WordSpec with MustMatchers with WebBrowser {
+
+  implicit val webDriver: WebDriver = new ChromeDriver()
+  sys.addShutdownHook(webDriver.close())
+
+  "The home page" must {
+    "have the correct title and content" in {
+      go to "https://www.gov.uk"
+
+      pageTitle mustEqual "Welcome to GOV.UK"
+      find(cssSelector("h1")).get.text mustEqual "Welcome to GOV.UK"
+
+      click on linkText("Benefits")
+      pageTitle mustEqual "Benefits - GOV.UK"
+
+      goBack()
+      pageTitle mustEqual "Welcome to GOV.UK"
+
+    }
+  }
 }
